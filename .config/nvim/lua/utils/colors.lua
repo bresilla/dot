036fdd3ab00f05f3ef825d84b2653d68da9092da
Trim Bresilla <trim.bresilla@gmail.com>
2025-01-23 03:07:26 +0100

    feat: enhance UI with new plugins and updated color themes
    - Add `neoscroll.nvim` plugin configuration to lazy-lock
    - Introduce new `neoscroll.lua` plugin file with scrolling configurations
    - Create a new `telescope.lua` plugin file with default setup and key mappings
    - Update color values in `colors.lua` for various UI elements
    - Modify color theme in `wal-theme.json` with updated background and element colors
    
    Signed-off-by: Trim Bresilla <trim.bresilla@gmail.com>

--- .config/nvim/lua/utils/colors.lua
+++ .config/nvim/lua/utils/colors.lua
@@ -31,6 +31,9 @@ function mycolors(theme)
         c["r" .. (i - 1)] = hsl(color)
     end
 
+    ac_d = c.r237
+    ac_l = c.r251
+
     local highlights = lush(function()
         return {
             -- Normal { bg = dark and c.r0.lighten(10) or c.r0.darken(10), fg = c.r15 },
@@ -40,14 +43,14 @@ function mycolors(theme)
             BufferDefaultCurrent { bg = c.r0, fg = c.r1, gui = "bold" },
             BufferDefaultCurrentSign { bg = c.r0, fg = c.r1.saturate(10) },
             BufferDefaultCurrentSignRight { bg = c.r0, fg = c.r1.saturate(10) },
-            BufferDefaultInactive {  bg = dark and c.r0.lighten(10) or c.r0.darken(10) },
-            BufferDefaultInactiveSign { bg = BufferDefaultInactive.bg, fg = c.r0 },
-            BufferDefaultInactiveSignRight { bg = BufferDefaultInactive.bg, fg = c.r0 },
-            BufferDefaultVisible { bg = BufferDefaultInactive.bg },
-            BufferDefaultVisibleSign { bg = BufferDefaultInactive.bg , fg = c.r0 },
-            BufferDefaultVisibleSignRight { bg = BufferDefaultInactive.bg, fg = c.r0 },
-            BufferTabpageFill { bg = BufferDefaultInactive.bg },
-            BufferTabpagesSep { bg = BufferDefaultInactive.bg },
+            BufferDefaultInactive {  bg = c.r237 },
+            BufferDefaultInactiveSign { bg = c.r237, fg = c.r0 },
+            BufferDefaultInactiveSignRight { bg = c.r237, fg = c.r0 },
+            BufferDefaultVisible { bg = c.r237 },
+            BufferDefaultVisibleSign { bg = c.r237 , fg = c.r0 },
+            BufferDefaultVisibleSignRight { bg = c.r237, fg = c.r0 },
+            BufferTabpageFill { bg = c.r237 },
+            BufferTabpagesSep { bg = c.r237 },
 
 
             ------- DASHBOARD -------
@@ -65,18 +68,82 @@ function mycolors(theme)
 
 
             ------- NvimTree -------
-            NvimTreeNormal { bg = BufferDefaultInactive.bg },
+            NvimTreeNormal { bg = c.r237 },
             NvimTreeCursorLine { bg = c.r0 },
             WinSeparator { fg = c.r0 },
 
 
             ------- STATUS-LINE -------
-            StatusLine { bg = BufferDefaultInactive.bg, fg = c.r1 },
+            StatusLine { bg = c.r237, fg = c.r1 },
             StatusLineNC { bg = c.r0, fg = c.r1 },
             ElNormal { bg = c.r1, fg = c.r0, gui = "bold" },
             ElNormal2 { bg = c.r0, fg = c.r1, gui = "bold" },
             ElInsert { bg = c.r0, fg = c.r1, gui = "bold" },
             ElFileType { bg = c.r1, fg = c.r0, gui = "bold" },
+
+            ------- INDENTATIONS -------
+            IndentLine { bg = c.r237 },
+            IndentOdd { bg = c.r237, fg = c.r0 },
+            IndentEven { bg = c.r237, fg = c.r236 },
+
+            ------- SEARCH -------
+            Search { bg = c.r1, fg = c.r0 },
+            HlSearchCur { bg = c.r1, fg = c.r0 },
+            HlSearchLensCur { bg = c.r0, fg = c.r1 },
+            HlSearchLens { bg = c.r0, fg = c.r1 },
+            CursorWord { bg = c.r1, fg = c.r0 },
+            CursorJump { bg = c.r0, fg = c.r1 },
+
+
+            ------- TELESCOPE -------
+            TelescopeBorder { fg = c.r1 },
+
+            -------- COMPLETION MENU -------
+            Pmenu { bg = c.r0, fg = c.r15 },
+            PmenuSel { bg = c.r237, fg = c.r1, gui = "bold" },
+            PmenuSbar { bg = c.r237 },
+            PmenuThumb { bg = c.r237 },
+            NormalFloat { bg = c.r0, fg = c.r15, gui = "bold" },
+
+
+
+            ------- SYNTAX -------
+            Constant       { fg = hsl("#de8ff6") }, -- (preferred) any constant
+            String         { fg = c.r1 }, --   a string constant: "this is a string"
+            Character      { String }, --  a character constant: 'c', '\n'
+            Number         { fg = hsl("#8eafff") }, --   a number constant: 234, 0xff
+            Boolean        { Constant }, --  a boolean constant: TRUE, false
+            Float          { Number }, --    a floating point constant: 2.3e10
+
+            Identifier     { fg = hsl("#eefeee") }, -- (preferred) any variable name
+            Function       { fg = hsl("#a7aeff") }, -- function name (also: methods for classes)
+
+            Statement      { fg = hsl("#cdacfc") }, -- (preferred) any statement
+            -- Conditional    { }, --  if, then, else, endif, switch, etc.
+            -- Repeat         { }, --   for, do, while, etc.
+            -- Label          { }, --    case, default, etc.
+            -- Operator       { }, -- "sizeof", "+", "*", etc.
+            -- Keyword        { }, --  any other keyword
+            -- Exception      { }, --  try, catch, throw
+
+            PreProc        { fg = hsl("#ffcbfb") }, -- (preferred) generic Preprocessor
+            -- Include        { }, --  preprocessor #include
+            -- Define         { }, --   preprocessor #define
+            -- Macro          { }, --    same as Define
+            -- PreCondit      { }, --  preprocessor #if, #else, #endif, etc.
+
+            Type           { fg = hsl("#f4af6f") }, -- (preferred) int, long, char, etc.
+            -- StorageClass   { }, -- static, register, volatile, etc.
+            -- Structure      { }, --  struct, union, enum, etc.
+            -- Typedef        { }, --  A typedef
+
+            Special        { fg = hsl("#eeef9f") }, -- (preferred) any special symbol
+            -- SpecialChar    { }, --  special character in a constant
+            -- Tag            { }, --    you can use CTRL-] on this
+            -- Delimiter      { }, --  character that needs attention
+            -- SpecialComment { }, -- special things inside a comment
+            -- Debug          { }, --    debugging statements
+
 
         }
     end)
