78f55fe26ff67cb03b405a531098e209de4a5415
Trim Bresilla <trim.bresilla@gmail.com>
2025-02-09 20:10:44 +0100

    style: enhance terminal usability with improved tmux configuration
    - Update SSH alias to use a simpler command
    - Add new tmux navigation configuration with key mappings for moving between panes
    - Modify terminal settings for improved compatibility in tmux
    - Change color scheme settings in zed theme for better visibility and aesthetics
    
    Signed-off-by: Trim Bresilla <trim.bresilla@gmail.com>

--- .config/nvim/lua/plugins/navigator.lua
+++ .config/nvim/lua/plugins/navigator.lua
@@ -1,45 +1,75 @@
 return {
-    -- {
-    --     'numToStr/Navigator.nvim',
-    --     config = function()
-    --         local navigator = require('Navigator')
-    --         navigator.setup({
-    --             auto_save = 'current'
-    --         })
-    --
-    --         vim.keymap.set('n', '<C-l>', [[<CMD>lua require('Navigator').right()<CR>]], {silent = true })
-    --         vim.keymap.set('i', '<C-l>', [[<CMD>lua require('Navigator').right()<CR>]], {silent = true })
-    --         -- vim.keymap.set('n', '<C-Right>', [[<CMD>lua require('Navigator').right()<CR>]], {silent = true })
-    --         -- vim.keymap.set('i', '<C-Right>', [[<CMD>lua require('Navigator').right()<CR>]], {silent = true })
-    --
-    --         vim.keymap.set('n', '<C-h>', [[<CMD>lua require('Navigator').left()<CR>]], {silent = true })
-    --         vim.keymap.set('i', '<C-h>', [[<CMD>lua require('Navigator').left()<CR>]], {silent = true })
-    --         -- vim.keymap.set('n', '<C-Left>', [[<CMD>lua require('Navigator').left()<CR>]], {silent = true })
-    --         -- vim.keymap.set('i', '<C-Left>', [[<CMD>lua require('Navigator').left()<CR>]], {silent = true })
-    --
-    --         vim.keymap.set('n', '<C-j>', [[<CMD>lua require('Navigator').down()<CR>]], {silent = true })
-    --         vim.keymap.set('i', '<C-j>', [[<CMD>lua require('Navigator').down()<CR>]], {silent = true })
-    --         -- vim.keymap.set('n', '<C-Down>', [[<CMD>lua require('Navigator').down()<CR>]], {silent = true })
-    --         -- vim.keymap.set('i', '<C-Down>', [[<CMD>lua require('Navigator').down()<CR>]], {silent = true })
-    --
-    --         vim.keymap.set('n', '<C-k>', [[<CMD>lua require('Navigator').up()<CR>]], {silent = true })
-    --         vim.keymap.set('i', '<C-k>', [[<CMD>lua require('Navigator').up()<CR>]], {silent = true })
-    --         -- vim.keymap.set('n', '<C-Up>', [[<CMD>lua require('Navigator').up()<CR>]], {silent = true })
-    --         -- vim.keymap.set('i', '<C-Up>', [[<CMD>lua require('Navigator').up()<CR>]], {silent = true })
-    --     end
-    -- },
-    { 'alexghergh/nvim-tmux-navigation', config = function()
+    {
+        "aserowy/tmux.nvim",
+        config = function()
+            vim.keymap.set("n", "<C-h>", [[<cmd>lua require("tmux").move_left()<cr>]], { noremap = true, silent = true })
+            vim.keymap.set("n", "<C-l>", [[<cmd>lua require("tmux").move_right()<cr>]], { noremap = true, silent = true })
+            vim.keymap.set("n", "<C-k>", [[<cmd>lua require("tmux").move_up()<cr>]], { noremap = true, silent = true })
+            vim.keymap.set("n", "<C-j>", [[<cmd>lua require("tmux").move_down()<cr>]], { noremap = true, silent = true })
 
-        local nvim_tmux_nav = require('nvim-tmux-navigation')
 
-            nvim_tmux_nav.setup {
-                disable_when_zoomed = true -- defaults to false
-            }
+            return require("tmux").setup({
+                copy_sync = {
+                    -- enables copy sync. by default, all registers are synchronized.
+                    -- to control which registers are synced, see the `sync_*` options.
+                    enable = true,
 
-            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+                    -- ignore specific tmux buffers e.g. buffer0 = true to ignore the
+                    -- first buffer or named_buffer_name = true to ignore a named tmux
+                    -- buffer with name named_buffer_name :)
+                    ignore_buffers = { empty = false },
+
+                    -- TMUX >= 3.2: all yanks (and deletes) will get redirected to system
+                    -- clipboard by tmux
+                    redirect_to_clipboard = false,
+
+                    -- offset controls where register sync starts
+                    -- e.g. offset 2 lets registers 0 and 1 untouched
+                    register_offset = 0,
+
+                    -- overwrites vim.g.clipboard to redirect * and + to the system
+                    -- clipboard using tmux. If you sync your system clipboard without tmux,
+                    -- disable this option!
+                    sync_clipboard = true,
+
+                    -- synchronizes registers *, +, unnamed, and 0 till 9 with tmux buffers.
+                    sync_registers = true,
+
+                    -- synchronizes registers when pressing p and P.
+                    sync_registers_keymap_put = true,
+
+                    -- synchronizes registers when pressing (C-r) and ".
+                    sync_registers_keymap_reg = true,
+
+                    -- syncs deletes with tmux clipboard as well, it is adviced to
+                    -- do so. Nvim does not allow syncing registers 0 and 1 without
+                    -- overwriting the unnamed register. Thus, ddp would not be possible.
+                    sync_deletes = true,
+
+                    -- syncs the unnamed register with the first buffer entry from tmux.
+                    sync_unnamed = true,
+                },
+                navigation = {
+                    -- cycles to opposite pane while navigating into the border
+                    cycle_navigation = true,
+
+                    -- enables default keybindings (C-hjkl) for normal mode
+                    enable_default_keybindings = true,
+
+                    -- prevents unzoom tmux when navigating beyond vim border
+                    persist_zoom = false,
+                },
+                resize = {
+                    -- enables default keybindings (A-hjkl) for normal mode
+                    enable_default_keybindings = true,
+
+                    -- sets resize steps for x axis
+                    resize_step_x = 1,
+
+                    -- sets resize steps for y axis
+                    resize_step_y = 1,
+                }
+            }) 
         end
     }
 }
