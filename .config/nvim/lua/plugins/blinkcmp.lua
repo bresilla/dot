8f8f6bf94d574df7ea3f91f7dd13eaaaff3c84f9
Trim Bresilla <trim.bresilla@gmail.com>
2025-02-15 12:32:17 +0100

    feat: refactor configuration and enhance script utilities
    - Add new aliases for `bino` and `bins` in the aliases file
    - Create a new configuration file for Alacritty with various settings
    - Update the versions and URLs for multiple binaries in the bin config file
    - Remove the obsolete sys.json configuration file
    - Modify Hyprland configuration to change gap settings
    - Update key bindings in Hyprland to use Alacritty instead of Kitty
    - Change font settings in Kitty configuration
    - Update the blinkcmp Lua plugin file with commented-out configurations
    - Introduce new font files for codicon, fluents, and nonicon
    - Enhance the screenshot capture script with better handling of OCR options
    - Improve the lule_colors script to handle color transfers across multiple devices

--- .config/nvim/lua/plugins/blinkcmp.lua
+++ .config/nvim/lua/plugins/blinkcmp.lua
@@ -37,6 +37,34 @@ local function lspkind_config()
     })
 end
 
+-- vim_item.kind = ({
+--                 Text = "  ",
+--                 Method = "  ",
+--                 Function = "  ",
+--                 Constructor = "  ",
+--                 Field = "  ",
+--                 Variable = "  ",
+--                 Class = " ﴯ ",
+--                 Interface = "  ",
+--                 Module = "  ",
+--                 Property = " ﰠ ",
+--                 Unit = "  ",
+--                 Value = "  ",
+--                 Enum = "  ",
+--                 Keyword = "  ",
+--                 Snippet = "  ",
+--                 Color = "  ",
+--                 File = "  ",
+--                 Reference = "  ",
+--                 Folder = "  ",
+--                 EnumMember = "  ",
+--                 Constant = "  ",
+--                 Struct = "  ",
+--                 Event = "  ",
+--                 Operator = "  ",
+--                 TypeParameter = "  "
+--             })
+
 return {{
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
