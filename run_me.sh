a1cf14018074e3ffa11df544cd70c2d4f0afdae9
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-10 13:48:32 +0100

    feat: Add Nix installation and update bin handling
    - Remove xinitrc from the symlinked dotfiles
    - Update bin installation to check for existing `bin` command
    - Ensure `bin` command is run without reinstalling if it already exists
    - Add Nix installation using the recommended install script
    - Create `/nix` directory and set ownership if it does not exist before Nix installation
    - Prevent Nix installation if `nix-env` command is already present

--- run_me.sh
+++ run_me.sh
@@ -13,19 +13,30 @@ done
 
 echo 'export ZDOTDIR="$HOME/.config/zsh"' > ~/.zshenv
 
-for item in "$ENVY"/.{profile,xinitrc,winitrc}; do
+for item in "$ENVY"/.{profile,winitrc}; do
     rm -rf "$HOME/$(basename "$item")"
     ln -sf "$item" "$HOME/"
 done
 
+# Install bin
 BINDIR="$HOME/.local/bin"
-
 echo "export PATH=$BINDIR:\$PATH" | sudo tee /etc/profile.d/envy.sh
 echo 'for f in /etc/profile.d/*.sh; do [[ -r $f ]] && source "$f"; done' | sudo tee -a /etc/zsh/zshrc
 echo 'for f in /etc/profile.d/*.sh; do [[ -r $f ]] && source "$f"; done' | sudo tee -a /etc/bash.bashrc
 echo "set -gx PATH $BINDIR \$PATH" | sudo tee /etc/fish/conf.d/envy.fish
+if [ ! command -v bin &> /dev/null ]; then
+    curl -sL https://github.com/marcosnils/bin/releases/download/v0.24.0/bin_0.24.0_linux_amd64 -o bin
+    chmod +x bin
+    ./bin ensure
+else
+    bin ensure
+fi
 
-# wget -q https://github.com/marcosnils/bin/releases/download/v0.24.0/bin_0.24.0_linux_amd64 -O bin
-# chmod +x bin
-
-mkdir -p "$BINDIR" && ./bin ensure
+# Install Nix
+if [ ! command -v nix-env &> /dev/null ]; then
+    if [ ! -d "/nix" ]; then
+        sudo mkdir /nix
+        chown -R $USER /nix
+    fi
+    sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+fi
